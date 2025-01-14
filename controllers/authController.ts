f6f4dd933d9fe577bc7c335253820a856372cbee
KBP09 <kanakbpandey2023@gmail.com>
2025-01-14 23:11:10 +0530

    Auth: role setting and login done
    Need to workon investors model

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,6 +1,25 @@
 import prisma from "../DB/db.config";
 import bcrypt from 'bcrypt';
-import { Request, Response } from 'express';
+import dotenv from 'dotenv'
+import * as jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+dotenv.config();
+export const authenticatToken = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        (req as any).user = decoded; 
+        next();
+    } catch (err) {
+        res.status(403).json({ error: 'Invalid or expired token.' });
+    }
+}
 
 export const signup = async (req: Request, res: Response): Promise<any> => {
     const { email, password } = req.body;
@@ -28,5 +47,61 @@ export const signup = async (req: Request, res: Response): Promise<any> => {
     }
     catch (error) {
         res.status(500).json({ error: "Error during signup" });
+    }
+}
+
+export const setRole = async (req: Request, res: Response): Promise<any> => {
+    const { userId, role } = req.body;
+
+    if (!["INVESTOR", "STARTUP_OWNER"].includes(role)) {
+        return res.status(400).json({ error: "Invalid role!" });
+    }
+    try {
+        await prisma.user.update({
+            where: { id: userId },
+            data: { role },
+        });
+
+        return res.status(200).json({ message: `Role set to ${role}` });
+    }
+    catch (error) {
+        return res.status(500).json({ error: 'Error updating role' });
+    }
+}
+
+export const login = async (req: Request, res: Response): Promise<any> => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+        })
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid email' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+
+        const accessToken = jwt.sign(
+            { id: user.id, email: user.email, role: user.role },
+            process.env.JWT_SECRET as string,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        );
+
+        res.status(200).json({
+            message: "Login Successful",
+            accessToken,
+            user: {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            },
+        });
+    }
+    catch (error) {
+        res.status(500).json({ error: "Error during login " });
     }
 }
--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/authController'
+import { signup,setRole,login } from '../controllers/authController'
 
 const authRoute = express.Router();
 
 authRoute.post("/signup",signup);
+authRoute.post("/setRole",setRole);
+authRoute.post("/login",login);
 
 export default authRoute;
