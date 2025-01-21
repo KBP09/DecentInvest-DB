6b01830136e64c952f128498713f8cc111ae2bf6
KBP09 <kanakbpandey2023@gmail.com>
2025-01-21 18:13:39 +0530

    Added create startup feature
    now CEO can create their multiple startups

--- controllers/profileController.ts
+++ controllers/profileController.ts
@@ -14,6 +14,7 @@ export const getProfile = async (req: Request, res: Response): Promise<any> => {
             include: {
                 ceoProfile: true,
                 investorProfile: true,
+                startups: true,
             }
         });
 
@@ -22,10 +23,12 @@ export const getProfile = async (req: Request, res: Response): Promise<any> => {
         }
 
         let profile = null;
-        if (user.role === "STARTUP_OWNER" && user.ceoProfile){
+        let startupDetails = null;
+        if (user.role === "STARTUP_OWNER" && user.ceoProfile) {
             profile = user.ceoProfile;
+            startupDetails = user.startups;
         }
-        else if(user.role === "INVESTOR" && user.investorProfile){
+        else if (user.role === "INVESTOR" && user.investorProfile) {
             profile = user.investorProfile;
         }
 
@@ -33,9 +36,49 @@ export const getProfile = async (req: Request, res: Response): Promise<any> => {
             return res.status(404).json({ error: "Profile not found" });
         }
 
-        return res.status(200).json({ profile });
+        return res.status(200).json({ profile,startupDetails });
 
     } catch (error) {
         res.status(500).json({ error: "Error fetching profile" });
+    }
+}
+
+export const createStartup = async (req: Request, res: Response): Promise<any> => {
+    const { userId, ceoName, name, description, fundingGoal, websiteLink } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            include: { ceoProfile: true }
+        });
+
+        if (!user || !user.ceoProfile) {
+            return res.status(403).json({ error: "You must be a CEO to create a startup." });
+        }
+
+        const newStartup = await prisma.startup.create({
+            data: {
+                name: name,
+                description: description,
+                fundingGoal: fundingGoal,
+                websiteLink: websiteLink,
+                ceoName: ceoName,
+                ownerId: userId
+            }
+        })
+
+        await prisma.cEOProfile.update({
+            where: { userId: userId },
+            data: {
+                startupsCreated: { increment: 1 },
+            },
+        })
+
+        return res.status(201).json({ message: 'Startup created successfully', newStartup });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error creating startup' });
     }
 }
--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import authRoute from './authRoute'
-import { getProfile } from '../controllers/profileController';
+import { getProfile, createStartup } from '../controllers/profileController';
+import { authenticateToken } from '../controllers/authController';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/auth", authRoute);
-app.use("/getProfile",getProfile);
+app.use("/getProfile", authenticateToken, getProfile);
+app.use("/create-startup", authenticateToken, createStartup);
 
 export default app;
