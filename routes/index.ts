e2a3a753d642b16e9a09ab929fecaa5ee460dfc2
KBP09 <kanakbpandey2023@gmail.com>
2025-01-24 18:12:22 +0530

    Routes: cors added
    Signed-off-by: KBP09 <kanakbpandey2023@gmail.com>

--- routes/index.ts
+++ routes/index.ts
@@ -2,8 +2,16 @@ import express from 'express'
 import authRoute from './authRoute'
 import { getProfile, createStartup } from '../controllers/profileController';
 import { authenticateToken } from '../controllers/authController';
+import cors from 'cors';
 
 const app = express();
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 
