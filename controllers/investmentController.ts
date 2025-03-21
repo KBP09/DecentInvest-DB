49c713dd6185a47d1e4d9f78b4c2eb2b9ec69829
KBP09 <kanakbpandey2023@gmail.com>
2025-03-21 23:53:44 +0530

    Investment: invest function created to add investment details in database
    Signed-off-by: KBP09 <kanakbpandey2023@gmail.com>

--- controllers/investmentController.ts
+++ controllers/investmentController.ts
@@ -2,6 +2,42 @@ import prisma from "../DB/db.config";
 import dotenv from 'dotenv';
 import { Request, Response } from "express";
 
+export const invest = async (req: Request, res: Response): Promise<any> => {
+    const { investorId, startupId, amount, equityTokens } = req.body;
+    try {
+        const investor = await prisma.investorProfile.findUnique({
+            where: {
+                id: investorId,
+            }
+        });
+
+        const startup = await prisma.startup.findUnique({
+            where: {
+                id: startupId,
+            }
+        });
+
+        if (!investor || !startup) {
+            return res.status(404).json({ error: "Investor or Startup not found" });
+        }
+
+        const investment = await prisma.investment.create({
+            data: {
+                investorId: investorId,
+                startupId: startupId,
+                amount: amount,
+                equityToken: equityTokens,
+                status: "Pending",
+            }
+        });
+
+        return res.status(200).json({ success: true, investment });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
 export const investments = async (req: Request, res: Response): Promise<any> => {
     const { investorId } = req.body;
     try {
