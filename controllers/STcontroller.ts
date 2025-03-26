0f2d348b4149b5096fa5af4063c03e9c80bcfac4
KBP09 <kanakbpandey2023@gmail.com>
2025-03-26 22:57:58 +0530

    SecurityToken: calculation logic added to calculate total recieved security tokens
    Signed-off-by: KBP09 <kanakbpandey2023@gmail.com>

--- controllers/STcontroller.ts
+++ controllers/STcontroller.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const storeSecurityToken = async (req: Request, res: Response): Promise<any> => {
     const { startupId, ticker, tokenName, assetId, totalSupply, ownerWallet } = req.body;
-    
+
     try {
         const token = await prisma.securityToken.create({
             data: {
@@ -19,11 +19,39 @@ export const storeSecurityToken = async (req: Request, res: Response): Promise<a
             }
         });
 
-        console.log("Stored Security Token:", token); 
+        console.log("Stored Security Token:", token);
         return res.status(200).json({ securityToken: token });
 
-    } catch (error:any) {
-        console.error("Database Error:", error); 
+    } catch (error: any) {
+        console.error("Database Error:", error);
         return res.status(500).json({ error: error.message || "Something went wrong" });
     }
 };
+
+export const calculateTokens = async (req: Request, res: Response): Promise<any> => {
+    const { startupId, amount } = req.body;
+    try {
+        const startup = await prisma.startup.findUnique({
+            where: { id: startupId },
+            select: {
+                equity: true,
+                fundingGoal: true
+            }
+        });
+
+        if (!startup) {
+            return res.status(404).json({ error: "Startup not found" });
+        }
+
+        const totalEquityTokens = 100;
+
+        const { equity, fundingGoal } = startup;
+
+        const tokenRecieved = (amount / fundingGoal) * (equity / 100) * totalEquityTokens;
+
+        return res.status(200).json({ tokensReceived: tokenRecieved.toFixed(2) });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
