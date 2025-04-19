fc8e268e208c59021b3e2ea6e1178e948818299a
KBP09 <kanakbpandey2023@gmail.com>
2025-04-19 16:44:19 +0530

    Transaction: investment transaction fully completed
    Signed-off-by: KBP09 <kanakbpandey2023@gmail.com>

--- controllers/transactionService.ts
+++ controllers/transactionService.ts
@@ -66,7 +66,7 @@ export const transaction = async (req: Request, res: Response): Promise<any> => 
         }
 
         const gasEstimateAddress = type === "investment" ? "0xb6d6d6d0c620075311562c89e22964f87079d373" : toAddress;
-        
+
         const transactionCost = await getTransactionCost(url, fromAddress, gasEstimateAddress, amount, contractAddress);
 
         if (nativeToken?.symbol !== currency) {
@@ -114,15 +114,393 @@ export const transaction = async (req: Request, res: Response): Promise<any> => 
             }];
 
             const investAbi = [{
-                name: "invest",
-                type: "function",
-                inputs: [
-                    { name: "_startupId", type: "uint256" },
-                    { name: "_amount", type: "uint256" }
-                ]
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "_startupId",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint128",
+                        "name": "_amount",
+                        "type": "uint128"
+                    }
+                ],
+                "name": "invest",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
             }];
 
-            
+            const investmentAbiFull = [
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "_admin",
+                            "type": "address"
+                        },
+                        {
+                            "internalType": "address",
+                            "name": "_usdcToken",
+                            "type": "address"
+                        }
+                    ],
+                    "stateMutability": "nonpayable",
+                    "type": "constructor"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "owner",
+                            "type": "address"
+                        }
+                    ],
+                    "name": "OwnableInvalidOwner",
+                    "type": "error"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "account",
+                            "type": "address"
+                        }
+                    ],
+                    "name": "OwnableUnauthorizedAccount",
+                    "type": "error"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "uint256",
+                            "name": "startupId",
+                            "type": "uint256"
+                        },
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "startupWallet",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint128",
+                            "name": "amount",
+                            "type": "uint128"
+                        }
+                    ],
+                    "name": "FundsReleased",
+                    "type": "event"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "uint256",
+                            "name": "startupId",
+                            "type": "uint256"
+                        },
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "investor",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint128",
+                            "name": "amount",
+                            "type": "uint128"
+                        }
+                    ],
+                    "name": "Invested",
+                    "type": "event"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "previousOwner",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "newOwner",
+                            "type": "address"
+                        }
+                    ],
+                    "name": "OwnershipTransferred",
+                    "type": "event"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "uint256",
+                            "name": "startupId",
+                            "type": "uint256"
+                        },
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "investor",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint128",
+                            "name": "amount",
+                            "type": "uint128"
+                        }
+                    ],
+                    "name": "Refunded",
+                    "type": "event"
+                },
+                {
+                    "anonymous": false,
+                    "inputs": [
+                        {
+                            "indexed": true,
+                            "internalType": "uint256",
+                            "name": "startupId",
+                            "type": "uint256"
+                        },
+                        {
+                            "indexed": true,
+                            "internalType": "address",
+                            "name": "wallet",
+                            "type": "address"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint256",
+                            "name": "fundingGoal",
+                            "type": "uint256"
+                        },
+                        {
+                            "indexed": false,
+                            "internalType": "uint256",
+                            "name": "deadline",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "StartupAdded",
+                    "type": "event"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "_wallet",
+                            "type": "address"
+                        },
+                        {
+                            "internalType": "uint128",
+                            "name": "_fundingGoal",
+                            "type": "uint128"
+                        },
+                        {
+                            "internalType": "uint64",
+                            "name": "_deadline",
+                            "type": "uint64"
+                        }
+                    ],
+                    "name": "addStartup",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "_startupId",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "getTotalFunded",
+                    "outputs": [
+                        {
+                            "internalType": "uint128",
+                            "name": "",
+                            "type": "uint128"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "_startupId",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "uint128",
+                            "name": "_amount",
+                            "type": "uint128"
+                        }
+                    ],
+                    "name": "invest",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "_startupId",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "isFundingGoalMet",
+                    "outputs": [
+                        {
+                            "internalType": "bool",
+                            "name": "",
+                            "type": "bool"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "owner",
+                    "outputs": [
+                        {
+                            "internalType": "address",
+                            "name": "",
+                            "type": "address"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "_startupId",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "refundInvestors",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "_startupId",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "releaseFunds",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "renounceOwnership",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "startupCounter",
+                    "outputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "",
+                            "type": "uint256"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "",
+                            "type": "uint256"
+                        }
+                    ],
+                    "name": "startups",
+                    "outputs": [
+                        {
+                            "internalType": "address",
+                            "name": "wallet",
+                            "type": "address"
+                        },
+                        {
+                            "internalType": "uint128",
+                            "name": "fundingGoal",
+                            "type": "uint128"
+                        },
+                        {
+                            "internalType": "uint128",
+                            "name": "totalFunded",
+                            "type": "uint128"
+                        },
+                        {
+                            "internalType": "uint64",
+                            "name": "deadline",
+                            "type": "uint64"
+                        },
+                        {
+                            "internalType": "uint8",
+                            "name": "status",
+                            "type": "uint8"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "address",
+                            "name": "newOwner",
+                            "type": "address"
+                        }
+                    ],
+                    "name": "transferOwnership",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "usdc",
+                    "outputs": [
+                        {
+                            "internalType": "contract IERC20",
+                            "name": "",
+                            "type": "address"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                }
+            ]
+
             const approveData = web3.eth.abi.encodeFunctionCall(usdcAbi[0], [investmentContractAddress, amountInWei]);
 
             const approveTx: any = {
@@ -139,7 +517,10 @@ export const transaction = async (req: Request, res: Response): Promise<any> => 
             const signedApprove = await web3.eth.accounts.signTransaction(approveTx, privateKey);
             const approveReceipt = await web3.eth.sendSignedTransaction(signedApprove.rawTransaction!);
 
-           
+            const investmentContract = new web3.eth.Contract(investmentAbiFull, investmentContractAddress);
+            const startup = await investmentContract.methods.startups(contractId).call();
+            console.log("Startup:", startup);
+
             const investData = web3.eth.abi.encodeFunctionCall(investAbi[0], [
                 contractId,
                 amountInWei
@@ -147,7 +528,7 @@ export const transaction = async (req: Request, res: Response): Promise<any> => 
 
             const investTx: any = {
                 from: fromAddress,
-                to: investmentContractAddress, 
+                to: investmentContractAddress,
                 data: investData,
                 gasPrice: await web3.eth.getGasPrice(),
                 nonce: await web3.eth.getTransactionCount(fromAddress, "pending"),
@@ -162,7 +543,7 @@ export const transaction = async (req: Request, res: Response): Promise<any> => 
             await prisma.transaction.create({
                 data: {
                     fromAddress,
-                    toAddress: investmentContractAddress, 
+                    toAddress: investmentContractAddress,
                     amount,
                     currency,
                     transactionHash: investReceipt.transactionHash.toString(),
